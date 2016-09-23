535fd0e5396d1dc946f0996629aa0dc97ec66e25
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-23 00:03:03 +0000

    version 1.0.1

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,12 @@
 
+# 1.0.1
+
+* Fix formatting of `Collate` fields, they always start at a new line now.
+
+* Fix formatting of `Authors@R` fields, when changed.
+
+* Keep trailing space after the `:` character, see #14
+
 # 1.0.0
 
 First public release.
--- R/str.R
+++ R/str.R
@@ -69,7 +69,7 @@ format.DescriptionDependencyList <- function(x, ...) {
 
 format.DescriptionCollate <- function(x, ...) {
   paste0(
-    blue(x$key), ":\n",
+    blue(x$key), ":",
     deparse_collate(parse_collate(color_bad(x)))
   )
 }
@@ -92,7 +92,7 @@ format.DescriptionAuthorsAtR <- function(x, mode = c("file", "screen"),
 
   } else {
     paste0(
-      blue(x$key), ":\n",
+      blue(x$key), ":",
       sub("\n$", "", deparse_authors_at_r(xx))
     )
   }
